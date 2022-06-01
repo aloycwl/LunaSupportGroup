39216e30b37685f9fd7e7e2bff0212a54cc96d56
Aloysius Chan <aloycwl@gmail.com>
2022-06-01 15:06:03 +0800

    Update generative.js

--- js/generative.js
+++ js/generative.js
@@ -1,98 +1,102 @@
 var Background = [
-    'Green',
-    'Pink',
-    'Brown',
-    'Dark Gray',
+    'Coral',
+    'Beech',
+    'Elm',
+    'Frog',
+    'Pine',
+    'Teal',
+    'Slate',
     'Purple',
-    'Gray',
-    'Yellow',
-    'Red',
-    'Blue',
-  ],
-  Body = [
-    'Blue',
-    'Pink',
-    'Light Gray',
-    'Green',
-    'Red',
-    'Dark Gray',
+    'Peach',
     'Brown',
-    'Purple',
-    'Yellow',
   ],
-  Headwear = ['Black Headphones', 'White Headphones', 'Beret', 'Tophat'],
-  Brow = ['Furrowed', 'Raised', 'Normal'],
-  Eyes = ['Sore', 'Normal', 'Crazy', 'Tired', 'Blur', 'Happy', 'Flame'],
-  Beak = ['Open', 'Sad', 'Gold Teeth', 'Tounge Out', 'Wide'],
-  mAccessories = ['Black Glasses', 'Gold Glasses', 'Monacle'],
-  mClothes = [
-    'Black Tattoo',
-    'White Tattoo',
-    'Red Shirt',
-    'Purple Shirt',
-    'Blue Shirt',
-    'Green Shirt',
-    'Purple Suit',
-    'Red Suit',
-    'Black Suit',
-    'Blue Suit',
+  Body = ['Cool'],
+  Hat = ['Cowboy', 'Party Hearts', 'Party Crescents', 'Floppy'],
+  Clothes = ['Sweater', 'Vest', 'Eskimo', 'Doctor'],
+  Neckwear = ['Furrowed', 'Loser Chain', 'Sailor Bow'],
+  Hand = ['Poker Cards', 'Blank', 'Lighted Cigarette', 'Ice Cream'],
+  Glasses = ['Cool', 'Star', 'Flower', 'Leaf'],
+  Ballon = [
+    'Type 1',
+    'Type 2',
+    'Type 3',
+    'Type 4',
+    'Type 5',
+    'Type 6',
+    'Type 7',
+    'Type 8',
+    'Type 9',
+    'Type 10',
+    'Type 11',
+    'Type 12',
+    'Type 13',
+    'Type 14',
+    'Type 15',
+    'Type 16',
+    'Type 17',
+    'Type 18',
+    'Type 19',
+    'Type 20',
+    'Type 21',
+    'Type 22',
+    'Type 23',
+    'Type 24',
+    'Type 25',
+    'Type 26',
+    'Type 27',
+    'Type 28',
+    'Type 29',
+    'Type 30',
+    'Type 31',
+    'Type 32',
+    'Type 33',
+    'Type 34',
+    'Type 35',
+    'Type 36',
+    'Type 37',
+    'Type 38',
+    'Type 39',
+    'Type 40',
+    'Type 41',
+    'Type 42',
+    'Type 43',
+    'Type 44',
+    'Type 45',
+    'Type 46',
+    'Type 47',
+    'Type 48',
+    'Type 49',
+    'Type 50',
   ],
-  fMask = ['Blue-Gold', 'Red-Purple', 'Red-Yellow', 'Gold'],
-  fClothes = [
-    'White Tattoo',
-    'Black Tattoo',
-    'Purple Flower Shirt',
-    'Blue Flower Shirt',
-    'Gray Flower Shirt',
-    'Pink Flower Shirt',
-    'Yellow Dress',
-    'Purple Dress',
-    'Blue Dress',
-    'Red Dress',
-  ],
-  fAccessories = ['Earrings', 'Necklace'],
-  fFlower = ['Red-Purple', 'Yellow-Orange', 'Purple-Blue'],
-  Teacup = ['Singapore'],
-  s = 340;
+  s = 1458;
 async function dd(s1, s2, s3, s4) {
   r = ran(s3.length);
   if (r + s2 <= s3.length)
     return new Promise((resolve) => {
       txt += `{"trait_type":"${s1}","value":"${s3[r]}"},`;
       img = new Image();
-      img.src = `https://aloycwl.github.io/twc_frontend/img/gen1.png`;
+      img.src = `https://aloycwl.github.io/lsg_frontend/img/lsgsprite.png`;
       img.crossOrigin = 'Anonymous';
       img.onload = function () {
-        cd.drawImage(img, r * s, s4 * s, s, s, 0, 0, s, s);
+        cd.drawImage(img, r * s, s4 * s, s, s, 0, 0, 350, 350);
         resolve();
       };
     });
 }
 async function drawLCA() {
-  $('#nfts').append(
-    `<canvas id="can${cnt}"width="${s}"height="${s}"></canvas>`
-  );
+  $('#nfts').append(`<canvas id="can${cnt}"width="350"height="350"></canvas>`);
   c = document.getElementById(`can${cnt}`);
   cd = c.getContext('2d');
   cd.clearRect(0, 0, 350, 350);
   sex = ran(2);
   txt = '"attributes":[';
-  await dd('Background', 3, Background, 0);
-  await dd('Body', 0, Body, 2);
-  await dd('Headwear', 2, Headwear, 1);
-  await dd('Brow', 0, Brow, 3);
-  await dd('Eyes', 0, Eyes, 4);
-  if (sex > 0) {
-    await dd('Clothes', 2, mClothes, 5);
-    await dd('Accessories', 1, mAccessories, 6);
-  } else {
-    await dd('Clothes', 2, fClothes, 7);
-    await dd('Accessories', 1, fAccessories, 8);
-    await dd('Flower', 2, fFlower, 9);
-    await dd('Mask', 2, fMask, 10);
-  }
-  await dd('Beak', 0, Beak, 11);
-  await dd('Teacup', 0, Teacup, 12);
+  await dd('Background', 0, Background, 1);
+  await dd('Body', 0, Body, 0);
+  await dd('Hat', 0, Hat, 2);
+  await dd('Clothes', 0, Clothes, 4);
+  await dd('Neckwear', 0, Neckwear, 3);
+  await dd('Hand', 0, Hand, 5);
+  await dd('Glasses', 0, Glasses, 6);
   txt = txt.substring(0, txt.length - 1) + ']';
   cnt++;
 }
